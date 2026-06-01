d9556b74d4e3f91f1b44582bb4b09cc1c7861547
Aleksandar Ristic <storiaca@gmail.com>
2026-06-01 11:31:56 +0200

    Add states image

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,10 @@
 import ProjectCards from "./ProjectCards";
-
+import states from "../../assets/states.png";
 const Projects = () => {
   return (
     <section className="projects" id="projects">
       <ProjectCards />
+      <img src={states} alt={"States"} />
     </section>
   );
 };
